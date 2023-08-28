1a5bb70d7225adda8918a52b06c83431dd78068f
aisha kh <akhaldi@andrew.cmu.edu>
2023-08-28 18:18:59 +0300

    Translated to TS and fixed some errors

--- src/rewards/index.js
+++ src/rewards/index.js
@@ -1,80 +1,134 @@
-'use strict';
-
-const util = require('util');
-
-const db = require('../database');
-const plugins = require('../plugins');
-
-const rewards = module.exports;
-
-rewards.checkConditionAndRewardUser = async function (params) {
-    const { uid, condition, method } = params;
-    const isActive = await isConditionActive(condition);
-    if (!isActive) {
-        return;
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
     }
-    const ids = await getIDsByCondition(condition);
-    let rewardData = await getRewardDataByIDs(ids);
-    rewardData = await filterCompletedRewards(uid, rewardData);
-    rewardData = rewardData.filter(Boolean);
-    if (!rewardData || !rewardData.length) {
-        return;
-    }
-    const eligible = await Promise.all(rewardData.map(reward => checkCondition(reward, method)));
-    const eligibleRewards = rewardData.filter((reward, index) => eligible[index]);
-    await giveRewards(uid, eligibleRewards);
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
-
-async function isConditionActive(condition) {
-    return await db.isSetMember('conditions:active', condition);
-}
-
-async function getIDsByCondition(condition) {
-    return await db.getSetMembers(`condition:${condition}:rewards`);
-}
-
-async function filterCompletedRewards(uid, rewards) {
-    const data = await db.getSortedSetRangeByScoreWithScores(`uid:${uid}:rewards`, 0, -1, 1, '+inf');
-    const userRewards = {};
-
-    data.forEach((obj) => {
-        userRewards[obj.value] = parseInt(obj.score, 10);
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-
-    return rewards.filter((reward) => {
-        if (!reward) {
-            return false;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const util = __importStar(require("util"));
+const db = __importStar(require("../database"));
+const plugins = __importStar(require("../plugins"));
+// interface RewardWithScore extends Reward {
+//     score: number;
+// }
+function isConditionActive(condition) {
+    return __awaiter(this, void 0, void 0, function* () {
+        /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+        return yield db.isSetMember('conditions:active', condition);
+    });
+}
+function getIDsByCondition(condition) {
+    return __awaiter(this, void 0, void 0, function* () {
+        /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+        return yield db.getSetMembers(`condition:${condition}:rewards`);
+    });
+}
+function getRewardDataByIDs(ids) {
+    return __awaiter(this, void 0, void 0, function* () {
+        /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+        return yield db.getObjects(ids.map((id) => `rewards:id:${id}`));
+    });
+}
+function filterCompletedRewards(
+/* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+uid, rewards) {
+    return __awaiter(this, void 0, void 0, function* () {
+        /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+        const data = yield db.getSortedSetRangeByScoreWithScores(`uid:${uid}:rewards`, 0, -1, 1, '+inf');
+        const userRewards = {};
+        /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+        data.forEach((obj) => {
+            /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+            userRewards[obj.value] = parseInt(obj.score, 10);
+        });
+        return rewards.filter((reward) => {
+            if (!reward) {
+                return false;
+            }
+            const claimable = parseInt(reward.claimable.toString(), 10);
+            return (claimable === 0 ||
+                (!userRewards[reward.id] || userRewards[reward.id] < claimable));
+        });
+    });
+}
+function checkCondition(reward, method) {
+    return __awaiter(this, void 0, void 0, function* () {
+        /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+        if (method.constructor && method.constructor.name !== 'AsyncFunction') {
+            method = util.promisify(method);
         }
-
-        const claimable = parseInt(reward.claimable, 10);
-        return claimable === 0 || (!userRewards[reward.id] || userRewards[reward.id] < reward.claimable);
+        /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+        const value = yield method();
+        const bool = yield plugins.hooks.fire(`filter:rewards.checkConditional:${reward.conditional}`, { left: value, right: reward.value });
+        return bool;
     });
 }
-
-async function getRewardDataByIDs(ids) {
-    return await db.getObjects(ids.map(id => `rewards:id:${id}`));
+function giveRewards(uid, rewards) {
+    return __awaiter(this, void 0, void 0, function* () {
+        /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+        const rewardData = yield getRewardsByRewardData(rewards);
+        for (let i = 0; i < rewards.length; i++) {
+            /* eslint-disable no-await-in-loop */
+            /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+            yield plugins.hooks.fire(`action:rewards.award:${rewards[i].rid}`, { uid: uid, reward: rewardData[i] });
+            yield db.sortedSetIncrBy(`uid:${uid}:rewards`, 1, rewards[i].id.toString());
+        }
+    });
 }
-
-async function getRewardsByRewardData(rewards) {
-    return await db.getObjects(rewards.map(reward => `rewards:id:${reward.id}:rewards`));
+const rewards = {};
+/* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+rewards.checkConditionAndRewardUser = function (params) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const { uid, condition, method } = params;
+        const isActive = yield isConditionActive(condition);
+        if (!isActive) {
+            return;
+        }
+        const ids = yield getIDsByCondition(condition);
+        let rewardData = yield getRewardDataByIDs(ids);
+        rewardData = yield filterCompletedRewards(uid, rewardData);
+        rewardData = rewardData.filter(Boolean);
+        if (!rewardData || !rewardData.length) {
+            return;
+        }
+        const eligible = yield Promise.all(rewardData.map((reward) => __awaiter(this, void 0, void 0, function* () { return yield checkCondition(reward, method); })));
+        const eligibleRewards = rewardData.filter((reward, index) => eligible[index]);
+        yield giveRewards(uid, eligibleRewards);
+    });
+};
+function getRewardsByRewardData(rewards) {
+    return __awaiter(this, void 0, void 0, function* () {
+        /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+        return yield db.getObjects(rewards.map((reward) => `rewards:id:${reward.id}:rewards`));
+    });
 }
-
-async function checkCondition(reward, method) {
-    if (method.constructor && method.constructor.name !== 'AsyncFunction') {
-        method = util.promisify(method);
-    }
-    const value = await method();
-    const bool = await plugins.hooks.fire(`filter:rewards.checkConditional:${reward.conditional}`, { left: value, right: reward.value });
-    return bool;
-}
-
-async function giveRewards(uid, rewards) {
-    const rewardData = await getRewardsByRewardData(rewards);
-    for (let i = 0; i < rewards.length; i++) {
-        /* eslint-disable no-await-in-loop */
-        await plugins.hooks.fire(`action:rewards.award:${rewards[i].rid}`, { uid: uid, reward: rewardData[i] });
-        await db.sortedSetIncrBy(`uid:${uid}:rewards`, 1, rewards[i].id);
-    }
-}
-
+rewards;
+/* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
 require('../promisify')(rewards);
---
+++ src/rewards/index.ts
@@ -0,0 +1,122 @@
+import * as util from 'util';
+import * as db from '../database';
+import * as plugins from '../plugins';
+
+interface Reward {
+    id: number;
+    conditional: string;
+    value: string | number | boolean;
+    claimable: number;
+    rid: string;
+}
+
+// interface RewardWithScore extends Reward {
+//     score: number;
+// }
+
+async function isConditionActive(condition: string): Promise<boolean> {
+    /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+    return await db.isSetMember('conditions:active', condition);
+}
+
+async function getIDsByCondition(condition: string): Promise<string[]> {
+    /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+    return await db.getSetMembers(`condition:${condition}:rewards`);
+}
+
+async function getRewardDataByIDs(ids: string[]): Promise<Reward[]> {
+    /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+    return await db.getObjects(ids.map((id) => `rewards:id:${id}`));
+}
+
+async function filterCompletedRewards(
+    /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+    uid: number,
+    rewards: Reward[]
+): Promise<Reward[]> {
+    /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+    const data = await db.getSortedSetRangeByScoreWithScores(
+        `uid:${uid}:rewards`,
+        0,
+        -1,
+        1,
+        '+inf'
+    );
+    const userRewards: { [key: string]: number } = {};
+    /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+    data.forEach((obj) => {
+        /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+        userRewards[obj.value] = parseInt(obj.score, 10);
+    });
+
+    return rewards.filter((reward) => {
+        if (!reward) {
+            return false;
+        }
+
+        const claimable = parseInt(reward.claimable.toString(), 10);
+        return (
+            claimable === 0 ||
+            (!userRewards[reward.id] || userRewards[reward.id] < claimable)
+        );
+    });
+}
+type MResult = boolean;
+
+async function checkCondition(reward: Reward, method: () => Promise<MResult>): Promise<boolean> {
+    /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+    if (method.constructor && method.constructor.name !== 'AsyncFunction') {
+        method = util.promisify(method);
+    }
+    /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+    const value = await method();
+    const bool = await plugins.hooks.fire<boolean>(`filter:rewards.checkConditional:${reward.conditional}`, { left: value, right: reward.value });
+    return bool;
+}
+
+async function giveRewards(uid: number, rewards: Reward[]): Promise<void> {
+    /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+    const rewardData = await getRewardsByRewardData(rewards);
+    for (let i = 0; i < rewards.length; i++) {
+        /* eslint-disable no-await-in-loop */
+        /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+        await plugins.hooks.fire(`action:rewards.award:${rewards[i].rid}`, { uid: uid, reward: rewardData[i] });
+        await db.sortedSetIncrBy(`uid:${uid}:rewards`, 1, rewards[i].id.toString());
+    }
+}
+
+const rewards: any = {};
+/* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+rewards.checkConditionAndRewardUser = async function (params: {
+    uid: number;
+    condition: string;
+    method: () => Promise<any>;
+}): Promise<void> {
+    const { uid, condition, method } = params;
+    const isActive = await isConditionActive(condition);
+    if (!isActive) {
+        return;
+    }
+    const ids = await getIDsByCondition(condition);
+    let rewardData = await getRewardDataByIDs(ids);
+    rewardData = await filterCompletedRewards(uid, rewardData);
+    rewardData = rewardData.filter(Boolean);
+    if (!rewardData || !rewardData.length) {
+        return;
+    }
+    const eligible = await Promise.all(
+        rewardData.map(async (reward) => await checkCondition(reward, method))
+    );
+    const eligibleRewards = rewardData.filter((reward, index) => eligible[index]);
+    await giveRewards(uid, eligibleRewards);
+};
+
+async function getRewardsByRewardData(rewards: Reward[]): Promise<Reward[]> {
+    /* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+    return await db.getObjects(rewards.map((reward) => `rewards:id:${reward.id}:rewards`));
+}
+
+rewards; 
+
+/* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+require('../promisify')(rewards);
